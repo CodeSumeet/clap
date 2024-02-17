2487fbe0d9335300a798b19a4df25a698830e5d2
Sumeet <121306709+CodeSumeet@users.noreply.github.com>
2024-02-17 21:10:18 +0530

    Delete backend/dist directory

--- backend/dist/routes/message.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const message_controller_1 = require("../controllers/message.controller");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const router = (0, express_1.Router)();
-router.route("/send/:id").post(auth_middleware_1.isAuthenticated, message_controller_1.sendMessage);
-exports.default = router;
